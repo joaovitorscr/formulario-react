b2ecbb6684846d568205bb86ff6234c14ead3612
João Vitor <duzt@pop-os.localdomain>
2023-02-28 12:56:38 -0300

    Calculos principais, layout, design e atualização de resultado dinamicamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [altura, setAltura] = useState(0.0)
+  const [peso, setPeso] = useState(0)
+  const calcImc = () => {
+    return (peso / (altura * altura))
+  }
+  let imc = calcImc().toFixed(2)
+  if (imc === 'NaN') {
+    imc = ''
+  }
+
+  let classificationValue = ''
+  if (calcImc() < 18.50) {
+    classificationValue = 'Magreza - Grau de obesidade 0'
+  } else if (calcImc() >= 18.50 && calcImc() <= 24.99) {
+    classificationValue = 'Normal - Grau de obesidade 0'
+  } else if (calcImc() >= 25 && calcImc() <= 29.99) {
+    classificationValue = 'Sobrepeso - Grau de obesidade 1'
+  } else if (calcImc() >= 30 && calcImc() <= 39.99) {
+    classificationValue = 'Obesidade- Grau de obesidade 2'
+  } else if (calcImc() > 40) {
+    classificationValue = 'Obesidade Grave - Grau de obesidade 3'
+  }
 
   return (
-    <div className="App">
+    <div className='form'>
+      <h1 className='form--title'>Calculadora de IMC</h1>
+      <form className='formElement'>
+        <div className='formElement__items'>
+          <label className='formElement--label' htmlFor="altura">Altura:</label>
+          <input className='formElement--input' type="number" id='altura' onBlur={(e) => setAltura(e.target.value)}/> 
+        </div>
+        <div className='formElement__items'>
+          <label className='formElement--label' htmlFor="peso">Peso:</label>
+          <input className='formElement--input' type="number" id='peso' onBlur={(e) => setPeso(e.target.value)}/> 
+        </div>
+      </form>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Resultado:</h2>
+        <p>{ classificationValue }</p>
+        <p>{ imc }</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
